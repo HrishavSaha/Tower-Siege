36d51917ce8f9f53467fce3eac585bf7c230b6d5
HrishavSaha <76658821+HrishavSaha@users.noreply.github.com>
2021-03-15 21:01:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,6 +5,12 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 
+var bg;
+
+function preload(){
+  getTime();
+}
+
 function setup() {
   createCanvas(800,400);
   engine = Engine.create();
@@ -26,7 +32,11 @@ function setup() {
 }
 
 function draw() {
-  background("grey");
+  if(bg === 1){
+    background("white");
+  }else{
+    background("black");
+  }
   Engine.update(engine);
   ground.display();
   stand.display();
@@ -56,5 +66,19 @@ function keyPressed(){
   if(keyCode === 32){
     Matter.Body.setPosition(polygon.body,{x:200,y:200});
     slingshot.attach(polygon.body);
+  }
+}
+
+async function getTime(){
+  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var responseJSON = await response.json();
+  console.log(responseJSON.datetime);
+
+  var datetime = responseJSON.datetime;
+  var hour = datetime.slice(10,13);
+  if(hour>=06 && hour<=19){
+    bg = 1;
+  }else{
+    bg = 2;
   }
 }
